5f645d96150458504699612e218c12b6b1741d38
Ali-Raza <100505916+ali-raza-tech@users.noreply.github.com>
2023-10-10 18:57:13 +0500

    Add files via upload

---
+++ Landing-Page/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screen:{
+        "sm":"480px"
+      }
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
+    },
+  },
+  plugins: [ 
+    require('flowbite/plugin'),    
+]}
